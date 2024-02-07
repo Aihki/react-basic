8b91e7eaf036675cd22798554cd00f5ae1d19c24
aihki <laine.nestori.julius@gmail.com>
2024-02-07 17:36:33 +0200

    adding right link in readme

--- src/components/FeedRow.tsx
+++ src/components/FeedRow.tsx
@@ -1,5 +1,5 @@
 import {Link} from 'react-router-dom';
-import {MediaItem, MediaItemWithOwner} from '../types/DBTypes';
+import {MediaItemWithOwner} from '../types/DBTypes';
 
 const FeedRow = (props: {item: MediaItemWithOwner}) => {
   const {item} = props;

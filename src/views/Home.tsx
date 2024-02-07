d2d5a63d31afa7b8373e927007d666e567937336
aihki <laine.nestori.julius@gmail.com>
2024-02-07 14:00:03 +0200

    starting some custom-hooks

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,33 +1,13 @@
-import {MediaItem, MediaItemWithOwner, User} from '../types/DBTypes';
+
 import FeedRow from '../components/FeedRow';
-import { useEffect, useState } from 'react';
-import { fetchData } from '../lib/utils';
+import useBook from '../hooks/apiHooks';
+import { MediaItemWithOwner } from '../types/DBTypes';
 
 
 
 const Home = () => {
 
-
-  const [mediaArray, setMediaArray] = useState<MediaItemWithOwner[]>([]);
-  const getBook = async () =>{
-    try {
-      const bookItems = await fetchData<MediaItem[]>(import.meta.env.VITE_MEDIA_API + '/media');
-
-      const bookItemsWithOwner: MediaItemWithOwner[] = await Promise.all(bookItems.map(async (item) => {
-        const user = await fetchData<User>(import.meta.env.VITE_AUTH_API + '/users/' + item.user_id)
-        const bookWithOwner: MediaItemWithOwner = { ...item, username: user.username };
-        return bookWithOwner;
-      }));
-
-      setMediaArray(bookItemsWithOwner);
-      console.log('bookItems', bookItemsWithOwner)
-    } catch (error) {
-      console.error('Error fetching book data', error);
-    }
-  }
-useEffect(() => {
-  getBook();
-},[]);
+const mediaArray: MediaItemWithOwner[] = useBook();
 
   return (
     <>
